65e8e5d20e1260eba6ae87449631a6eaedcb781c
Felipe <macbookair@MacBook-Air-de-MacBook.local>
2020-03-29 10:39:30 -0300

    Todas as telas deste app

--- testDrive/Views/AgendamentoView.xaml.cs
+++ testDrive/Views/AgendamentoView.xaml.cs
@@ -7,11 +7,43 @@ namespace testDrive.Views
 {
     public partial class AgendamentoView : ContentPage
     {
+
+        public string Nome { get; set; }
+        public string Fone { get; set; }
+        public string Email { get; set; }
+
+        DateTime dataAgendamento = DateTime.Today;
+        public DateTime DataAgendamento
+        {
+            get
+            {
+                return dataAgendamento;
+            }
+            set
+            {
+                dataAgendamento = value;
+            }
+        }
+        public TimeSpan HoraAgendamento { get; set; }
+
         public AgendamentoView(Veiculos veiculo)
         {
             InitializeComponent();
 
             this.Title = veiculo.Veiculo;
+            this.BindingContext = this;
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            DisplayAlert("Dados Preenchidos", @"Nome: " + Nome + "\n" +
+                "Telefone: " + Fone + "\n" +
+                "E-mail: "+ Email + "\n"+
+                "Data Agendamento:"+ DataAgendamento + "\n" +
+                "Hora Agendamento: " + HoraAgendamento
+
+
+                , "Ok");
         }
     }
 }
--- testDrive/Views/DetalheView.xaml.cs
+++ testDrive/Views/DetalheView.xaml.cs
@@ -9,12 +9,98 @@ namespace testDrive.Views
     {
         private readonly Veiculos Veiculos;
 
+        private const decimal FREIO_ABS = 800;
+        private const decimal AR_CONDICIONADO = 1000;
+        private const decimal MP3_PALYER = 500;
+
+        public string TextFreio
+        {
+            get
+            {
+                return string.Format("Freio ABS R$ {0}", FREIO_ABS);
+            }
+        }
+
+        public string TextAr
+        {
+            get
+            {
+                return string.Format("Ar-condicionado R$ {0}", AR_CONDICIONADO);
+            }
+        }
+
+
+        public string TextMp3
+        {
+            get
+            {
+                return string.Format("MP3 Player R$ {0}", MP3_PALYER);
+            }
+        }
+
+        bool temFreioAbs;
+        public bool TemFreio {
+            get
+            {
+                return temFreioAbs;
+            }
+            set
+            {
+                temFreioAbs = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        bool temAr;
+        public bool TemAr
+        {
+            get
+            {
+                return temAr;
+            }
+            set
+            {
+                temAr = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        bool temMp3;
+        public bool TemMp3
+        {
+            get
+            {
+                return temMp3;
+            }
+            set
+            {
+                temMp3 = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        public string ValorTotal
+        {
+            get
+            {               
+                return string.Format("Valor Total: R$ {0}", Veiculos.Valor
+                    + (TemFreio ? FREIO_ABS : 0) + (temAr ? AR_CONDICIONADO : 0) +
+                    (TemMp3 ? MP3_PALYER : 0)) ;
+            }
+        }
+
         public DetalheView(Veiculos veiculo)
         {
             InitializeComponent();
             
             this.Veiculos = veiculo;
             this.Title = veiculo.Veiculo;
+
+
+            this.BindingContext = this;
         }
 
         void btnProximo_Clicked(System.Object sender, System.EventArgs e)
@@ -24,5 +110,6 @@ namespace testDrive.Views
             Navigation.PushAsync(new AgendamentoView(veiculoParam));
         }
 
+        
     }
 }
